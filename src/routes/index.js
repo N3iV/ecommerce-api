bb802bce567270146b59a89e9e4b7bac304e46b6
Vien Huynh <vien200203@gmail.com>
2023-05-31 23:26:17 +0700

    feat:factory pattern for product model

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ router.use(apiKey);
 router.use(permission("0000"));
 
 router.use("/v1/api", require("./access/index"));
+router.use("/v1/api/product", require("./product"));
 
 // router.get("/", (req, res) => {
 //   return res.status(200).json({k
