4c4f8f876003ae64b3bb402a8212295b613f7516
Vien Huynh <vien200203@gmail.com>
2023-06-10 20:57:46 +0700

    feat: enhance product model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const { default: slugify } = require("slugify");
 
 const DOCUMENT_NAME = "Product";
 const COLLECTION_NAME = "Products";
@@ -14,6 +15,8 @@ const productSchema = new Schema(
       required: true,
     },
     product_description: String,
+    product_slug: String,
+
     product_price: {
       type: Number,
       required: true,
@@ -35,12 +38,42 @@ const productSchema = new Schema(
       type: Schema.Types.Mixed,
       required: true,
     },
+    //more
+    product_ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be under 5.0"],
+      set: (val) => Number.round(val * 10) / 10,
+    },
+    product_variations: {
+      type: Array,
+      default: [],
+    },
+    isDraft: {
+      type: Boolean,
+      default: true,
+      index: true,
+      select: false,
+    },
+    isDraft: {
+      type: Boolean,
+      default: false,
+      index: true,
+      select: false,
+    },
   },
   {
     collection: COLLECTION_NAME,
     timestamps: true,
   }
 );
+
+//Document middleware. Run before .save() and .create()
+
+productSchema.pre("save", function (next) {
+  this.product_slug = slugify(this.product_name, { lower: true });
+});
 
 const clothingSchema = new Schema(
   {
